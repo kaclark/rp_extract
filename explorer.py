29ab7faa1938a5f14b6d504ffb98fa0805455fc4
Keanu Clark <da1andonlyone13@gmail.com>
2023-06-29 20:03:18 -0400

    cleaning away files I won't use

--- explorer.py
+++ explorer.py
@@ -2,7 +2,7 @@ from rp_extract import *
 from audiofile_read import *
 from rp_plot import *
 
-audiofile = "Kehlani_Gangsta_Arkane_remix.wav"
+audiofile = "../../songs/Kehlani_Gangsta_Arkane_remix.wav"
 samplerate, samplewidth, wavedata = audiofile_read(audiofile)
 extracted_features = rp_extract(wavedata,                            # the two-channel wave-data of the audio-file
                                 samplerate,                          # the samplerate of the audio-file
@@ -13,4 +13,4 @@ extracted_features = rp_extract(wavedata,                            # the two-c
                                 fluctuation_strength_weighting=True, # apply psycho-accoustic transformation
                                 skip_leadin_fadeout = 1,             # skip lead-in/fade-out. value = number of segments skipped
                                 step_width          = 1)
-plotrp(extracted_features['rp'], savefp=audiofile.split(".wav")[0] + ".png")
+plotrp(extracted_features['rp'], savefp="../test_rp.png")
