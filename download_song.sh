6ca5f4ee7b7bbb27dbf6766653a0ff6ad28a4524
geomlattice <markov.particle@gmail.com>
2024-03-16 18:50:21 +0000

    tested youtube-dl and rp_extract.py on pixel 3aXL Debian through termux

--- download_song.sh
+++ download_song.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #output Path not tested, but method works
-song_url=""
-yt-dlp -x --audio-format wav $song_url -o ./songs/$1
+song_url=$1
+song_path=$2
+yt-dlp -x --audio-format wav $song_url -o ./songs/$song_path
